0f461463719f44c96ec44ad4223959e6f1dc0813
SaulFernand0 <saulolivas@upeu.edu.pe>
2021-11-10 22:31:57 -0500

    v5.0

--- src/controllers/alumno.controller.js
+++ src/controllers/alumno.controller.js
@@ -6,7 +6,7 @@ const readAlumno = async (req, res) => {
         return res.status(200).json(response.rows);
     } catch (e) {
         console.log(e)
-        return res.status(500).json('Internal Server error...!');
+        return res.status(500).json('Internal Server error!');
     }
 }
 
@@ -16,10 +16,10 @@ const createAlumno = async (req, res) => {
         await pool.query('INSERT INTO alumno(nombres, apellidos, direccion, telefono, idescuela) VALUES ( $1, $2, $3, $4, $5);',
             [nombres, apellidos, direccion, telefono, idescuela]);
         return res.status(200).json(
-            `Alumno ${nombres} creado correctamente...!`);
+            `Alumno ${nombres} creado correctamente.`);
     } catch (e) {
         console.log(e);
-        return res.status(500).json('Internal Server error...!');
+        return res.status(500).json('Internal Server error!');
     }
 }
 
@@ -30,7 +30,7 @@ const readAlumnoID = async (req, res) => {
         return res.status(200).json(response.rows);
     } catch (e) {
         console.log(e)
-        return res.status(500).json('Internal Server error...!');
+        return res.status(500).json('Internal Server error!');
     }
 }
 
@@ -40,10 +40,11 @@ const updateAlumno = async (req, res) => {
         const { nombres, apellidos, direccion, telefono, idescuela } = req.body;
         const response = await pool.query('UPDATE alumno set nombres = $1, apellidos = $2, direccion = $3, telefono = $4, idescuela = $5 where idalumno = $6',
             [nombres, apellidos, direccion, telefono, idescuela, id]);
-        return res.status(200).json(response.rows);
+        return res.status(200).send(`El Alumno ${id} se ha actualizado correctamente.`);
+            
     } catch (e) {
         console.log(e)
-        return res.status(500).json('Internal Server error...!');
+        return res.status(500).json('Internal Server error!');
     }
 }
 
@@ -52,10 +53,10 @@ const deleteAlumno = async (req, res) => {
         const id = parseInt(req.params.id);
         await pool.query('delete from alumno where idalumno=$1', [id]);
         return res.status(200).json(
-            `Alumno ${id} eliminado correctamente...!`);
+            `Alumno ${id} eliminado correctamente.`);
     } catch (e) {
         console.log(e)
-        return res.status(500).json('Internal Server error...!');
+        return res.status(500).json('Internal Server error!');
     }
 }
 
